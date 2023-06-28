d839c484ef4e6b39b34e12e61b2f4450246ddeeb
XTR\haikal.rahmadi <haikal.rahmadi@xtremax.com>
2023-06-28 18:36:08 +0700

    test commit

---
+++ src/components/example/Example.tsx
@@ -0,0 +1,10 @@
+const Example = ({ title, subtitle }: { title: string; subtitle: string }) => {
+  return (
+    <>
+      <div className="text-4xl text-green">{title}</div>
+      <div className="text-2xl">{subtitle}</div>
+    </>
+  )
+}
+
+export default Example
