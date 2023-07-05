56e0475be15b99f46ccbf8ac9d96d645a8657ef7
XTR\haikal.rahmadi <haikal.rahmadi@xtremax.com>
2023-07-05 14:45:14 +0700

    feat: update aja

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,16 @@
+const Header = () => {
+  return (
+    <>
+      <div class="flex items-center justify-center flex-col">
+        <div class="tracking-wider">
+          <span class="text-xl font-extrabold">Hi 🧞‍♀️</span>
+          <span class="text-xl"> I’m Software Developer</span>
+        </div>
+        <h1 class="text-5xl font-extrabold text-black mb-10">M. Haikal Rahmadi</h1>
+        <button class="text-sm px-6 py-2 text-white rounded-full bg-gradient-to-tr from-violet-300 to-pink-400">Read More</button>
+      </div>
+    </>
+  )
+}
+
+export default Header
